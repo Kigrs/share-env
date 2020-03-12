4aeebd3b445cdaf09d175e96dad157b91fa7ffc6
kigrs <ki.299792458@gmail.com>
2020-03-12 21:51:53 +0900

    make it usefull to find symlinks to files in dotfiles directory.

--- setup.sh
+++ setup.sh
@@ -2,20 +2,27 @@
 
 home_dir=$HOME
 dotfiles_dir=$HOME/dotfiles
+SRCH_DPTH=5
 
 echo -e "# Make symlink by folloing command."
 
 echo -e "\n- New files -"
-for files in $(basename $(find $dotfiles_dir -name '.*' -maxdepth 1)) ; do
+for files in $(basename $(find $dotfiles_dir -mindepth 1 -maxdepth 1 -not -iwholename '*/.git*' -not -iwholename '*/dotfiles/setup.sh')) ; do
     
-    [ -L "$home_dir/$files" ] || echo "ln -s $dotfiles_dir/$files $home_dir/$files"
+    linkfile="$(find $home_dir -name "$files" -not -iwholename '*/.Trash/*' -not -iwholename '*/dotfiles/*' -not -iwholename '*/.local/*' -maxdepth $SRCH_DPTH 2>/dev/null)"
+    if [ ! -L "$linkfile" ];  then 
+      [ -z "$linkfile" ] && linkfile="<anywhere>" 
+      echo -e "\033[1mln -si $dotfiles_dir/$files $linkfile\033[m"
+    fi
     #ln -sf $dotfiles_dir/$files $home_dir/$files
 done
 
 echo -e "\n- Already symlink exists -"
-for files in $(basename $(find $dotfiles_dir -name '.*' -maxdepth 1)) ; do
+for files in $(basename $(find $dotfiles_dir -mindepth 1 -maxdepth 1 -not -iwholename '*/.git*')) ; do
     
-    [ -L "$home_dir/$files" ] && echo "ln -s $dotfiles_dir/$files $home_dir/$files"
+    linkfile="$(find $home_dir -name "$files" -type l -maxdepth $SRCH_DPTH 2>/dev/null)"
+    #[ -n "$linkfile" ] &&  echo -e "\033[0m$(ls -lG $linkfile)\033[m"
+    [ -n "$linkfile" ] &&  ls -lG $linkfile
     #ln -sf $dotfiles_dir/$files $home_dir/$files
 done
 
