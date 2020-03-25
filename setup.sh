c23043d800285a28ef14ecbe6a7b560947b079af
K.Igarashi <keisuke.igarashi1@ibm.com>
2020-03-25 17:21:57 +0900

    OSの種類によって環境設定ファイルを変更するよう構成を大きく変更しました。

--- setup.sh
+++ setup.sh
@@ -1,29 +1,35 @@
 #!/usr/bin/sh
 
+if [ "$(uname)" == 'Darwin' ]; then
+    OS_NAME='osx'
+elif [ "$(uname)" == 'Linux' ]; then
+    OS_NAME='linux'
+else
+    echo unknown os type.
+    exit 1
+fi
+
 home_dir=$HOME
-dotfiles_dir=$HOME/dotfiles
+dotfiles_dir=$HOME/dotfiles/$OS_NAME
 SRCH_DPTH=5
 
-echo -e "# Make symlink by folloing command."
+echo -e "# Make symlink by following command."
 
 echo -e "\n- New files -"
-for files in $(basename $(find $dotfiles_dir -mindepth 1 -maxdepth 3 -not -iwholename '*/.git*' -not -iwholename '*/dotfiles/setup.sh')) ; do
-    
+for files in $(basename $(find $dotfiles_dir -name ".*" -mindepth 1 -maxdepth 1 -not -iwholename '*/.git*' -not -iwholename '*/.DS_Store')) ; do
+
     linkfile="$(find $home_dir -name "$files" -not -iwholename '*/.Trash/*' -not -iwholename '*/dotfiles/*' -not -iwholename '*/.local/*' -maxdepth $SRCH_DPTH 2>/dev/null)"
     if [ ! -L "$linkfile" ];  then 
       [ -z "$linkfile" ] && linkfile="<anywhere>" 
       echo -e "\033[1mln -si $dotfiles_dir/$files $linkfile\033[m"
     fi
-    #ln -sf $dotfiles_dir/$files $home_dir/$files
 done
 
 echo -e "\n- Already symlink exists -"
-for files in $(basename $(find $dotfiles_dir -mindepth 1 -maxdepth 3 -not -iwholename '*/.git*')) ; do
+for files in $(basename $(find $dotfiles_dir -name ".*" -mindepth 1 -maxdepth 1 -not -iwholename '*/.git*' -not -iwholename '*/.DS_Store')) ; do
     
     linkfile="$(find $home_dir -name "$files" -type l -maxdepth $SRCH_DPTH 2>/dev/null)"
-    #[ -n "$linkfile" ] &&  echo -e "\033[0m$(ls -lG $linkfile)\033[m"
-    [ -n "$linkfile" ] &&  ls -lG $linkfile
-    #ln -sf $dotfiles_dir/$files $home_dir/$files
+    [ -n "$linkfile" ] &&  ls -lG $linkfile &&  echo -e "\033[1mln -si $dotfiles_dir/$files $linkfile\033[m"
 done
 
 exit 0
