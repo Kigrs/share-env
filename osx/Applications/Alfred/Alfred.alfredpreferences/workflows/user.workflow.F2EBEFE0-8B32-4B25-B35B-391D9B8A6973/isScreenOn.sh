f6ee007fb65a760f005c5155e6617bbbdf86eede
kigrs <ki.299792458@gmail.com>
2021-01-18 19:16:59 +0900

    timerアプリで画面オフの検出方法を変更して、クラムシェルモードに対応しました

--- osx/Applications/Alfred/Alfred.alfredpreferences/workflows/user.workflow.F2EBEFE0-8B32-4B25-B35B-391D9B8A6973/isScreenOn.sh
+++ osx/Applications/Alfred/Alfred.alfredpreferences/workflows/user.workflow.F2EBEFE0-8B32-4B25-B35B-391D9B8A6973/isScreenOn.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-if [ "`echo $(ioreg -c AppleBacklightDisplay) | grep '\"dsyp\"={\"min\"=0,\"max\"=2,\"value\"=2}'`" ]; then exit 0; else exit 1; fi
+if [ "`ioreg -n IODisplayWrangler | grep '\"DevicePowerState\"=4'`" ];then
+    exit 0
+else
+    exit 1
+fi
